f88275b7febdf4937b6633ba6e4e2db84037249a
USER <USER@DESKTOP-E0CM5Q7>
2022-06-14 16:18:20 +0900

    Apple의 타입을 double로 변경

--- src/practice/Apple.java
+++ src/practice/Apple.java
@@ -2,15 +2,16 @@ package practice;
 
 public class Apple {
 	
-	int a;
-	int b;
-	int c;
+	double a;
+	double b;
+	double c;
+	Float d;
 	
 	public Apple(int a ,int b ,int c) {
 		this.a=a;
 		this.b=b;
 		this.c=c;
-		 
+		 this.d = 0.123F;
 	}
 
 }
